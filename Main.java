b968fab5b76b848dfb5d9ee2a2019f0d721d747b
HaoXK <haoxuank@nycstudents.net>
2025-05-05 18:18:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,20 @@
+public class Main
+{
+  public static void main(String[] args) 
+  {
+    StepTracker tr =  new StepTracker(10000);
+    System.out.println(tr.activeDays());
+    System.out.println(tr.averageSteps());
+    tr.addDailyStep(9000);
+    tr.addDailyStep(5000);
+    System.out.println(tr.averageSteps());
 
+    tr.addDailyStep(13000);
+    System.out.println(tr.activeDays());
+    System.out.println(tr.averageSteps());
+    tr.addDailyStep(23000);
+    tr.addDailyStep(1111);
+    System.out.println(tr.activeDays());
+    System.out.println(tr.averageSteps());
+  }
+}
